4c74396387dad08980a3beb9ecac59922acb1c1d
Gabriel Passos <gpassos@lendingclub.com>
2024-05-13 13:19:23 -0300

    feat: add matrix poc

--- matrix-poc/src/core.clj
+++ matrix-poc/src/core.clj
@@ -2,8 +2,12 @@
 
 (use 'clojure.core.matrix)
 
-(def my_matrix (shape [[1 2] [3 4]]))
+(def my_matrix (matrix [[1 2] [3 4]]))
+
+(def my_shape (shape [[1 2] [3 4]]))
 
 (defn -main []
-  (println my_matrix))
-
+  (println my_matrix)
+  (pm my_matrix)
+  (println my_shape)
+  (pm my_shape))
