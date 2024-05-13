c3007fdd65ecad8d8140ec7fb01ab244b078480c
Gabriel Passos <gpassos@lendingclub.com>
2024-05-13 13:54:15 -0300

    feat: update matrix

--- matrix-poc/src/core.clj
+++ matrix-poc/src/core.clj
@@ -7,7 +7,11 @@
 (def my_shape (shape [[1 2] [3 4]]))
 
 (defn -main []
+  (println "Matrix")
   (println my_matrix)
   (pm my_matrix)
+  (println(get-row my_matrix 0))
+
+  (println "Shape")
   (println my_shape)
   (pm my_shape))
