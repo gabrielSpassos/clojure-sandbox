e766936a6a1982c222295cc8201f0c787ba32238
Gabriel Passos <gpassos@lendingclub.com>
2024-05-20 10:28:19 -0300

    refact: refactor queen poc

--- queens-poc/src/core.clj
+++ queens-poc/src/core.clj
@@ -14,5 +14,42 @@
   true ;;todo: change to true
 )
 
+(defn is-safe? [matrix row column]
+  (let [queen? #(= % QUEEN)]
+    (letfn [(check-row [i]
+              (if (< i column)
+                (if (queen? (get-in matrix [row i]))
+                  false
+                  (recur (inc i)))
+                true))
+            (check-upper-diagonal [i j]
+              (if (and (>= i 0) (>= j 0))
+                (if (queen? (get-in matrix [i j]))
+                  false
+                  (recur (dec i) (dec j)))
+                true))
+            (check-lower-diagonal [i j]
+              (if (and (>= j 0) (< i (count matrix)))
+                (if (queen? (get-in matrix [i j]))
+                  false
+                  (recur (inc i) (dec j)))
+                true))]
+      (and (check-row 0)
+           (check-upper-diagonal row column)
+           (check-lower-diagonal row column)))))
+
+;; Usage example:
+(def QUEEN "Q")
+
+(def matrix [["." "." "." "."]
+             [QUEEN "." "." "."]
+             ["." "." "." "."]
+             ["." "." "." "."]])
+
+(def row 0)
+(def column 0)
+
+(is-safe? matrix row column)
+
 (defn -main []
   (println "N Queen Problem POC"))
