791f1ef57218eea48354d3dcdc0c3ad0ce5158c8
Gabriel Passos <gpassos@lendingclub.com>
2024-06-10 12:05:00 -0300

    feat: add code to if poc

--- if-poc/src/core.clj
+++ if-poc/src/core.clj
@@ -7,3 +7,15 @@
         false
         (recur (inc i)))
       true)))
+
+(defn inc-or-dec [row column direction]
+  (if (= :up direction) (dec row) (inc row))
+)
+
+(defn decision-with-recur [row column direction]
+  (loop [i row, j column]
+    (if (or (= 10 i) (= -10 i) (= -10 j))
+    true 
+    (recur (if (= direction :up) (dec i) (inc i))
+          (dec j)))))
+    
--- if-poc/test/core_test.clj
+++ if-poc/test/core_test.clj
@@ -11,3 +11,17 @@
   (t/testing "decision should return true"
     (t/is (= true (sut/decision valid-letters-case))))
 )
+
+(t/deftest inc-or-dec-tests
+  (t/testing "inc-or-dec should return dec row"
+    (t/is (= -1 (sut/inc-or-dec 0 0 :up))))
+  (t/testing "inc-or-dec should return inc row"
+    (t/is (= 1 (sut/inc-or-dec 0 0 :down))))
+)
+
+(t/deftest decision-with-recur-tests
+  (t/testing "decision-with-recur should return true"
+    (t/is (= true (sut/decision-with-recur 0 0 :up))))
+  (t/testing "decision-with-recur should return true"
+    (t/is (= true (sut/decision-with-recur 0 0 :down))))
+)
